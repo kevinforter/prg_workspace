eb3b5b44f63c3c814d6527067f0f83b2f54bb9a2
kevinforter <49223606+kevinforter@users.noreply.github.com>
2023-03-25 21:07:15 +0100

    New File

---
+++ git_project/src/ch/hslu/informatik/prg/project/g12/project_g12_02.java
@@ -0,0 +1,10 @@
+package ch.hslu.informatik.prg.project.g12;
+
+public class project_g12_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Fuckers");
+	}
+
+}
