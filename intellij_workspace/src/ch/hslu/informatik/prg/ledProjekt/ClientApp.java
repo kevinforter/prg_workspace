d9169937000053a22309b1525909d6c874713f5b
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-03-26 19:58:43 +0100

    added ein und ausschlaten

--- intellij_workspace/src/ch/hslu/informatik/prg/ledProjekt/ClientApp.java
+++ intellij_workspace/src/ch/hslu/informatik/prg/ledProjekt/ClientApp.java
@@ -20,14 +20,39 @@ public class ClientApp {
 
     private static void ledsOnOff() {
 
+        // 1. Eingabe
         System.out.print("Geben sie die Anzahl hinzuzufügenden LED Reihen an: ");
         int rows = sc.nextInt();
 
+        // 2. LED hinzufügen
+        Led[][] ledArr = null;
         if (rows > MAX_ROWS) {
             System.out.println("Die Anzahl der Reihen darf nicht größer als 32 sein.");
         } else {
-            service.add(rows);
-            service.pauseExecution(2000);
+            ledArr = service.add(rows);
         }
+
+        // 3. Pause
+        service.pauseExecution(2000);
+
+        // 4. Einschalten
+        for (int i = rows-1; i >= 0; i--) {
+            for (int j = ledArr.length-1; j >= 0; j--) {
+                (ledArr[i][j]).turnOn();
+            }
+        }
+
+        // 5. Pause
+        service.pauseExecution(250);
+
+        // 6. Ausschalten
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < ledArr.length; j++) {
+                (ledArr[i][j]).turnOff();
+            }
+        }
+
+        // 6. Pause
+        service.pauseExecution(250);
     }
 }
