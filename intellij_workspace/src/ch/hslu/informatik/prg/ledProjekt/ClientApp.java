6da8507b17626ab43d08432cdf1c6e110ee0b4e3
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-03-26 17:25:33 +0100

    ledsOnOff() adding leds

--- intellij_workspace/src/ch/hslu/informatik/prg/ledProjekt/ClientApp.java
+++ intellij_workspace/src/ch/hslu/informatik/prg/ledProjekt/ClientApp.java
@@ -4,14 +4,30 @@ import ch.hslu.prg.ledboard.proxy.Led;
 import ch.hslu.prg.ledboard.proxy.BoardService;
 import ch.hslu.prg.ledboard.proxy.LedColor;
 
+import java.util.Scanner;
+
 public class ClientApp {
 
-    private static final int MAX_ROWS = 32;
+    private static final BoardService service = new BoardService();
+    private static final Scanner sc = new Scanner(System.in);
+    private static final int MAX_ROWS = BoardService.MAX_ROWS;
+
     public static void main(String[] args) {
 
-        BoardService boardService = new BoardService();
+        ledsOnOff();
 
-        boardService.add(MAX_ROWS);
+    }
 
+    private static void ledsOnOff() {
+
+        System.out.print("Geben sie die Anzahl hinzuzufügenden LED Reihen an: ");
+        int rows = sc.nextInt();
+
+        if (rows > MAX_ROWS) {
+            System.out.println("Die Anzahl der Reihen darf nicht größer als 32 sein.");
+        } else {
+            service.add(rows);
+            service.pauseExecution(2000);
+        }
     }
 }
